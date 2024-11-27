9d6646f8ee480d4c731229cacce7e11de37ca38c
Youssef Hassan <yousefheroo028@gmail.com>
2024-11-28 00:01:11 +0200

    Update main.cpp
    Merged .h file and .cpp files.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,55 @@
 #include <iostream>
-#include "Headers.h"
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+class LabelGenerator {
+protected:
+    string label;
+    int counter;
+public:
+    explicit LabelGenerator(const string &text, int c = 1) {
+        label = text;
+        counter = c;
+    }
+
+    virtual string nextLabel() {
+        return label + to_string(counter++);
+    }
+};
+
+class FileLabelGenerator : public LabelGenerator {
+private:
+    ifstream file;
+public:
+    FileLabelGenerator(const string &text, int c, const string &fileName) : LabelGenerator(text, c) {
+        file.open(fileName);
+    }
+
+    string nextLabel() override {
+        string sentence;
+        if (file) {
+            while (getline(file, sentence, '\n')) {
+                cout << LabelGenerator::nextLabel() << ' ' << sentence << '\n';
+            }
+        }
+        return "";
+    }
+};
 
 int main() {
     cout << "Hello user, welcome to using our program...\n";
     cout << "\nChoose reading from file or generate unique labels: \n1- From file.\n2- Generate unique labels.\n";
-    char ope;
+    string ope;
     cin >> ope;
-    while (ope != '1' && ope != '2') {
+    while (ope != "1" && ope != "2") {
         cout << "Please, enter a valid input: ";
         cin.ignore();
         cin.clear();
         cin >> ope;
     }
-    if (ope == '1') {
+    if (ope == "1") {
         cin.ignore();
         cin.clear();
         cout << "Type the label you want: ";
@@ -23,9 +58,14 @@ int main() {
         cout << "\nType the beginning of the counter: ";
         int counter;
         cin >> counter;
-        FileLabelGenerator fileLabelNumbers(label, counter, "file.txt");
+        cin.ignore();
+        cin.clear();
+        cout << "\nType the name of the file: ";
+        string fileName;
+        getline(cin, fileName);
+        FileLabelGenerator fileLabelNumbers(label, counter, fileName);
         cout << fileLabelNumbers.nextLabel();
-    } else if (ope == '2') {
+    } else if (ope == "2") {
         cin.ignore();
         cin.clear();
         cout << "Type the label you want: ";
@@ -43,5 +83,6 @@ int main() {
             if (i < number_of_labels - 1) cout << '\n';
         }
     }
+    cout << endl, system("pause");
     return 0;
 }
